78d7515029a98b560966b39a97d8e859bccb89e0
guidoni <guidoni@gmail.com>
2026-04-23 15:55:58 -0300

    V 0.1.1

--- samha.backend/src/main/java/com/samha/config/WebConfig.java
+++ samha.backend/src/main/java/com/samha/config/WebConfig.java
@@ -5,24 +5,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/*
-@Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
- 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-            .allowedOrigins(                
-                "http://localhost:4200",
-                            "https://172.16.32.16"
-            ) // A URL do  frontend Angular
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedHeaders("*")
-            .allowCredentials(true);
-    }
-}
-*/
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
@@ -35,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
                             "https://172.16.32.16",
                             "https://samha.colatina.ifes.edu.br/"
             )
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
     }
